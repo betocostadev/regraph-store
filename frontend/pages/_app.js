77b71066f46c119c1828a69929f5cb915c667fee
betocostadev <robertomarx@gmail.com>
2022-11-03 08:45:28 -0300

    adding single product page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -30,7 +30,7 @@ Regraph.getInitialProps = async function ({ Component, ctx }) {
     pageProps = await Component.getInitialProps(ctx)
   }
   pageProps.query = ctx.query
-  return pageProps
+  return { pageProps }
 }
 
 // Inject the Apollo client into the App
---
+++ frontend/pages/product/[id].js
@@ -0,0 +1,3 @@
+export default function Product({ query }) {
+  return <div>Single Product {query.id}</div>
+}
