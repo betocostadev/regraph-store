ede8831a68f218d0495487f50701e53de23c8e5f
betocostadev <robertomarx@gmail.com>
2022-11-17 08:00:28 -0300

    Added pagination query

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client'
 import styled from 'styled-components'
 import { ALL_PRODUCTS_QUERY } from '../lib/services/products'
 import Product from './Product'
+import { perPage } from '../config'
 
 const ProductsListStyles = styled.div`
   display: grid;
@@ -9,8 +10,13 @@ const ProductsListStyles = styled.div`
   grid-gap: 60px;
 `
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY)
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  })
   // console.log(data, error, loading)
   if (loading) return <p>Loading products...</p>
   if (error) return <p>Error: {error.message}</p>
